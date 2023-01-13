d4d9a2e88c5d7b321552a42ebaec3fcc4cf9dc77
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-13 14:45:11 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 interface Color {
-  red: number
-  blue: number
-  green: number
+  red: number;
+  blue: number;
+  green: number;
 }
 
 const color: Color = {
   red: 10,
   blue: 10,
   green: 10,
-}
+};
 
-console.log(color)
+console.log(color);
+
+export default color;
